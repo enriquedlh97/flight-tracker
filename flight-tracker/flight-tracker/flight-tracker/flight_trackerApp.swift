ba14a80ca4376ec185a4ffb5143f02191fcb2a2c
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-08 14:35:59 -0700

    Cteates project

---
+++ flight-tracker/flight-tracker/flight-tracker/flight_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  flight_trackerApp.swift
+//  flight-tracker
+//
+//  Created by Enrique Diaz de Leon on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct flight_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
