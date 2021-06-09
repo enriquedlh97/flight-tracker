c526ef78f62dafcea9ba0a6f17cd456e2fdd13a2
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-09 15:23:33 -0700

    Adds flisghtmodel and fetchdata fucntion

---
+++ flight-tracker/flight-tracker/flight-tracker/FlightModel.swift
@@ -0,0 +1,40 @@
+//
+//  FlightModel.swift
+//  flight-tracker
+//
+//  Created by Enrique Diaz de Leon on 6/9/21.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseFirestore
+
+class FlightModel: ObservableObject {
+    
+    @Published var flights = [Flight]()
+    private let db = Firestore.firestore()
+    
+    init() {
+        
+        fetchData()
+        
+    }
+    
+    func fetchData() {
+        
+        db.collection("flights").order(by: "departure_scheduled").addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.flights = documents.compactMap { queryDocumentSnapshot -> Flight? in
+                return try? queryDocumentSnapshot.data(as: Flight.self)
+                
+            }
+            
+        }
+        
+    }
+    
+}
