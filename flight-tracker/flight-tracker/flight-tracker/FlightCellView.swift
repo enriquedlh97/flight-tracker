8dfdda1a91f2f9121565e0c5880e2983ad7a5000
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-10 14:00:14 -0700

    Adds FlightCellView

---
+++ flight-tracker/flight-tracker/flight-tracker/FlightCellView.swift
@@ -0,0 +1,25 @@
+//
+//  FlightCellView.swift
+//  flight-tracker
+//
+//  Created by Enrique Diaz de Leon on 6/10/21.
+//
+
+import SwiftUI
+
+struct FlightCellView: View {
+    
+    var flight: Flight
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct FlightCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlightCellView(flight: Flight.dummy)
+    }
+}
