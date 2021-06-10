ba0ebead54e79cc7be2f9a7232bac9a8bf590311
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-10 08:57:36 -0700

    Adds extensions

---
+++ flight-tracker/flight-tracker/flight-tracker/extensions.swift
@@ -0,0 +1,12 @@
+//
+//  extensions.swift
+//  flight-tracker
+//
+//  Created by Enrique Diaz de Leon on 6/10/21.
+//
+
+import SwiftUI
+
+let apikey = "e479c62dbcc6e2de7e6585fe4dbc1ed7"
+let apiauth = ""
+let headers = ""
