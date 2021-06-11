cb3e8d8f8bbef18c0ccaa46662a544d3238305aa
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-10 18:23:00 -0700

    Adds filter to Flight List in FlightListView

--- flight-tracker/flight-tracker/flight-tracker/FlightListView.swift
+++ flight-tracker/flight-tracker/flight-tracker/FlightListView.swift
@@ -10,14 +10,26 @@ import SwiftUI
 struct FlightListView: View {    
     
     @StateObject var data: DataModel
+    @State var text = ""
     
     var body: some View {
         
         NavigationView {
             VStack {
-                List(data.flightsList, id: \.flight_number) { flight in
-                    NavigationLink(destination: FlightDetailView(flight: flight, data: data), label: {FlightCellView(flight: flight)})
+                SearchBarView(text: $text)
+                List {
+                    Section(
+                    header: Text("Live Flights"),
+                        content: {
+                            ForEach(data.flightsList.filter {$0.airline_name.contains(text) || $0.aircraft_iata.contains(text) || $0.aircraft_icao.contains(text) || $0.flight_number.contains(text) || $0.departure_airport.contains(text) || text.isEmpty} ?? [Flight.dummy], id: \.flight_number) { flight in
+                                NavigationLink(destination: FlightDetailView(flight: flight, data: data), label: {FlightCellView(flight: flight)})
+                            }
+                        })
                 }
+
+//                List(data.flightsList, id: \.flight_number) { flight in
+//                    NavigationLink(destination: FlightDetailView(flight: flight, data: data), label: {FlightCellView(flight: flight)})
+//                }
             }
             .listStyle(DefaultListStyle())
             //.padding(.bottom, 20)
