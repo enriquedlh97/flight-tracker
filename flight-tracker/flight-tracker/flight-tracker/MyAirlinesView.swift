4c42c18ed973d4c7c94dbd839d50441b366ecd33
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-11 16:30:42 -0700

    Updates MyAirlinesView

--- flight-tracker/flight-tracker/flight-tracker/MyAirlinesView.swift
+++ flight-tracker/flight-tracker/flight-tracker/MyAirlinesView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct MyAirlinesView: View {
+    
+    @StateObject var airlines: AirlineModel
+    @StateObject var data: DataModel
+    
     var body: some View {
-            VStack {
-                Text("Marked Airlines")
+        VStack {
+            List {
+                Section(
+                    header: Text("Airports"),
+                    content: {
+                        ForEach(airlines.airlines) { airline in
+                            NavigationLink(
+                                destination: AirlineDetailView(airlines: airlines, airline: airline, data: data),
+                                label: {
+                                    AirlineCellView(airline: airline)
+                                        .contextMenu {
+                                            Button {
+                                                airlines.removeData(airline: airline)
+                                            } label: {
+                                                HStack {
+                                                    Text("Delete")
+                                                    Image(systemName: "xmark.circle.fill")
+                                                }
+                                            }
+                                        }
+                                })
+                        }
+                    })
             }
+            .listStyle(PlainListStyle())
+        }
             .navigationBarTitle("Marked Airlines", displayMode: .inline)
             .navigationBarColor(UIColor(named: "ElectronBlue"), UIColor(named: "SwanWhite"))
             .toolbar {
@@ -28,6 +55,6 @@ struct MyAirlinesView: View {
 
 struct MyAirlinesView_Previews: PreviewProvider {
     static var previews: some View {
-        MyAirlinesView()
+        MyAirlinesView(airlines: AirlineModel(), data: DataModel())
     }
 }
