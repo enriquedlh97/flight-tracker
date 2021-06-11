dc65413b5e5f560c53941096ab1c48e9894a42d4
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-10 18:13:23 -0700

    Adds SearchBarView

---
+++ flight-tracker/flight-tracker/flight-tracker/SearchBarView.swift
@@ -0,0 +1,61 @@
+//
+//  SearchBarView.swift
+//  flight-tracker
+//
+//  Created by Enrique Diaz de Leon on 6/10/21.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @Binding var text: String
+    @State private var isEditing = false
+    
+    var body: some View {
+        HStack {
+            TextField("Search country", text: $text)
+                .padding(15)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .foregroundColor(Color("BlueGreen"))
+                .cornerRadius(15)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color("BlueGreen"))
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 15)
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }, label: {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(Color("BlueGreen"))
+                                    .padding(.trailing, 8)
+                            })
+                        }
+                    }
+                ).onTapGesture {
+                    self.isEditing = true
+                }
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }){
+                    Text("Cancel")
+                        .foregroundColor(Color("GreenBlue"))
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+        .padding(.horizontal, 6)
+    }
+}
+
+
